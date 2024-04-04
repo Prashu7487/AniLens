3924435c02f270796c889bfa7d60918efe480abd
Prashu7487 <pm7487912@gmail.com>
2024-04-05 01:41:26 +0530

    corrected basic files

---
+++ pages/overview.py
@@ -0,0 +1,7 @@
+import dash
+from dash import html
+
+layout = html.Div([
+    html.H1('Overview Page'),
+    html.P('This is the content of the overview page.')
+])
---
+++ pages/trends.py
@@ -0,0 +1,7 @@
+import dash
+from dash import html
+
+layout = html.Div([
+    html.H1('Trends Page'),
+    html.P('This is the content of the trends page.')
+])
