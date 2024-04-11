03fbdc5a6eca454e9d90c9470f342e644f0bf4df
Tushar Dupga <Tushardupga@gmail.com>
2024-04-11 15:33:25 +0530

    Added more graphs

--- pages/trends.py
+++ pages/trends.py
@@ -1,8 +1,61 @@
 import pandas as pd
 import geopandas as gpd
-from dash import dcc, html
+from dash import dcc, html, Input, Output, callback
 import numpy as np
 import plotly.express as px
+import os
+#
+# Episode Duration vs. Popularity Scatter Plot: Investigate the correlation between episode duration and anime
+# popularity, generating a scatter plot to identify patterns
+# in viewer preferences.
+
+def top_10_anime_based_on_most_genre():
+    # Load Data
+    data = pd.read_csv(os.path.abspath('./data/anime_cleaned.csv'))
+    # Create a bar chart which shows have most typeof genre
+    data['Genres'] = data['Genres'].apply(lambda x: x.split(', '))
+    data = data.explode('Genres')
+    # plot should be name vs how many genres it has
+    data = data.groupby('Name').size().reset_index(name='Count')
+    data = data.sort_values(by='Count', ascending=False)
+    data = data.head(10)
+    genre_fig = px.bar(data, x='Name', y='Count', title='Top 10 Anime based on Most Genre')
+    return genre_fig
+
+
+def ep_duration_vs_popularity_scatter_plot():
+    # Load Data
+    data = pd.read_csv(os.path.abspath('./data/anime_cleaned.csv'))
+    # Create a scatter plot
+    scatter_fig = px.scatter(data, x='Episodes', y='Popularity', title='Episode Duration vs Popularity',
+                             labels={'Episodes': 'Number of Episodes', 'Popularity': 'Popularity'})
+    return scatter_fig
+
+
+def rating_evoution_over_decade():
+    # Load Data
+    data = pd.read_csv(os.path.abspath('./data/anime_cleaned.csv'))
+    season_year = data['Premiered'].map(extract_season_year)
+    premiered_year = season_year.apply(lambda x: x[1])
+    data['Premiered'] = premiered_year
+    # convert premiered column to int
+    data['Premiered'] = data['Premiered'].astype('int')
+    # create bins for decades
+    bins = [1960, 1970, 1980, 1990, 2000, 2010, 2020]
+    labels = ['1960-1970', '1970-1980', '1980-1990', '1990-2000', '2000-2010', '2010-2020']
+    data['Decade'] = pd.cut(data['Premiered'], bins=bins, labels=labels, right=False)
+    # group data based on rated
+    # remove unknown values
+    data = data[data['Rating'] != 'Unknown']
+    data = data.groupby(['Decade', 'Rating']).size().reset_index(name='Count')
+    # create a pivot table
+    data = data.pivot(index='Decade', columns='Rating', values='Count')
+    data = data.fillna(0)
+    # create a line plot and each point should have different icon
+    symbol_sequence = ['circle', 'square', 'diamond', 'cross']
+    decade_fig = px.line(data, x=data.index, y=data.columns, title='Rating Evolution Over Decade', markers=True,
+                         symbol="Rating")
+    return decade_fig
 
 
 def extract_season_year(premiered):
@@ -14,8 +67,8 @@ def extract_season_year(premiered):
 
 
 def number_of_anime_released_per_year():
-    df_anime = pd.read_csv(r"D:\data\anime_cleaned.csv")
-    #count row of df_anime'
+    df_anime = pd.read_csv(os.path.abspath('./data/anime_cleaned.csv'))
+    # count row of df_anime'
     # print(df_anime.shape)
     # scores = df_anime['Score'][df_anime['Score'] != 'Unknown']
     # scores = scores.astype('float')
@@ -29,7 +82,6 @@ def number_of_anime_released_per_year():
     # df_anime = df_anime[df_anime['Type'] != 'Unknown']
     # #save into csv
     # df_anime.to_csv(r"D:\data\anime_cleaned.csv", index=False)
-    print(df_anime.shape)
 
     season_year = df_anime['Premiered'].map(extract_season_year)
     premiered_season = season_year.apply(lambda x: x[0])
@@ -37,34 +89,119 @@ def number_of_anime_released_per_year():
     filtered_premiered_season = premiered_season.dropna()
     fitered_premiered_year = premiered_Year.dropna()
     year_count = fitered_premiered_year.value_counts().sort_index()
-    #delete rows where premiered column is unknown
-    print(year_count)
+    # delete rows where premiered column is unknown
     return year_count
 
 
 # Load Data
-data = pd.read_csv(r"D:\data\anime_popularity.csv")
+data_popularity = pd.read_csv(os.path.abspath('./data/anime_popularity.csv'))
+anime_data = pd.read_csv(os.path.abspath('./data/anime_cleaned.csv'))
+anime = pd.read_csv(os.path.abspath('./data/anime.csv'))
+list_of_all_genres = anime['Genres'].apply(lambda x: x.split(', ')).explode().unique().tolist()
 
 # ... (Your code for loading data, etc.)
 
-fig = px.choropleth(data, locations="iso_alpha",
+fig = px.choropleth(data_popularity, locations="iso_alpha",
                     color="Anime",
-                    hover_data=["name","Anime", "Genre"],
+                    hover_data=["name", "Anime", "Genre"],
                     color_continuous_scale="plasma",
                     projection="orthographic")
 
 fig.update_layout(margin=dict(l=60, r=60, t=50, b=50),
-                  geo=dict(bgcolor= 'rgba(2,1,50,0)',
-                           showframe=True,oceancolor='deepskyblue'),
+                  geo=dict(bgcolor='rgba(2,1,50,0)',
+                           showframe=True, oceancolor='deepskyblue'),
                   )  # Customize this color
 # ... (Rest of your code)
 
 
-fig1 = px.line(x=number_of_anime_released_per_year().index, y=number_of_anime_released_per_year().values, labels={'x': 'Year', 'y': 'Number of Anime Released'})
+# fig1 = px.line(x=number_of_anime_released_per_year().index, y=number_of_anime_released_per_year().values,
+#                labels={'x': 'Year', 'y': 'Number of Anime Released'})
 # Assuming Dash setup exists ...
 layout = html.Div([dcc.Graph(figure=fig),
-                   #adding a graph to show the number of anime released per year
-                   html.H1('Number of Anime Released per Year'),
-                   html.P('This is the content of the trends page.'),
-                     dcc.Graph(figure=fig1)
+                   # adding a graph to show the number of anime released per year
+                   dcc.RadioItems(
+                       id='radio-items',
+                       options=[
+                           {'label': 'Completed', 'value': 'completed'},
+                           {'label': 'Watching', 'value': 'watching'},
+                           {'label': 'On hold', 'value': 'on_hold'},
+                           {'label': 'Plan to watch', 'value': 'plan_to_watch'},
+                           {'label': 'Dropped', 'value': 'dropped'},
+                           {'label': 'Favorite', 'value': 'favorite'},
+                       ],
+                       value="completed",
+                       labelStyle={'display': 'inline-block'}
+                   ),
+                   dcc.Graph(id='bar_graph'),
+                   # create dropdown to select genre
+                   dcc.Dropdown(
+                       id='genre-dropdown',
+                       options=list_of_all_genres,
+                       value=['Action'],
+                       multi=True
+                   ),
+                   dcc.Graph(id='genre-graph'),
+                   dcc.Graph(figure=rating_evoution_over_decade()),
+                   dcc.Graph(figure=ep_duration_vs_popularity_scatter_plot()),
                    ])
+
+
+@callback(
+    Output('bar_graph', 'figure'),
+    Input('radio-items', 'value')
+)
+def update_pie_chart(selected_value):
+    # load data using os module
+    data = pd.read_csv(os.path.abspath('./data/anime_cleaned.csv'))
+    map = {'completed': 'Completed', 'watching': 'Watching', 'on_hold': 'On-Hold', 'plan_to_watch': 'Plan to Watch',
+           'dropped': 'Dropped', 'favorite': 'Favorites'}
+    # filter data based on selected value and find percentage of each map value
+
+    filtered_data_status = data[['Name', 'Completed', 'Watching', 'On-Hold', 'Plan to Watch', 'Dropped', 'Favorites']]
+
+    # index,value= filtered_data_status['Name'], (filtered_data_status[map[selected_value]] / filtered_data_status[
+    #     ['Completed', 'Watching', 'On-Hold', 'Plan to Watch', 'Dropped']].sum(axis=1)) * 100
+    # map_index = {index[i]: value[i] for i in range(len(index))}
+    # map_fav = {index[i]: filtered_data_status['Favorites'][i] for i in range(len(index))}
+    # if selected_value == 'favorite':
+    #     # sort based on value inside map_fav
+    #     map_fav = dict(sorted(map_fav.items(), key=lambda item: item[1], reverse=True))
+    #     # get top 10 anime
+    #     map_fav = dict(list(map_fav.items())[:10])
+    #     figure = px.bar(x=list(map_fav.keys()), y=list(map_fav.values()), title=f'Top 10 Anime based on {map[selected_value]}')
+    # else:
+    #     #sort based on value inside map_index
+    #     map_index = dict(sorted(map_index.items(), key=lambda item: item[1], reverse=True))
+    #     #get top 10 anime
+    #     map_index = dict(list(map_index.items())[:10])
+    #     figure = px.bar(x=list(map_index.keys()), y=list(map_index.values()), title=f'Top 10 Anime based on {map[selected_value]}')
+    # return figure
+    # sort based on selected value and get top 10 anime
+    filtered_data_status = filtered_data_status.sort_values(by=map[selected_value], ascending=False)
+    filtered_data_status = filtered_data_status.head(10)
+    figure = px.bar(x=filtered_data_status['Name'], y=filtered_data_status[map[selected_value]],
+                    title=f'Top 10 Anime based on {map[selected_value]}')
+    return figure
+
+
+@callback(
+    Output('genre-graph', 'figure'),
+    Input('genre-dropdown', 'value')
+)
+def update_chart(selected_value):
+    # load data using os module
+    print(selected_value)
+    data = pd.read_csv(os.path.abspath('./data/anime_cleaned.csv'))
+    # calculate number of releases selcted genres according to year
+    season_year = data['Premiered'].map(extract_season_year)
+    premiered_year = season_year.apply(lambda x: x[1])
+    data['Premiered'] = premiered_year
+    # convert selected value to list
+    # if selected value is in particular anime's genres add it to list
+    # find all anime sthat have selected value in their genres
+    # below is wrong
+    data = data[data['Genres'].apply(lambda x: all(genre in x for genre in selected_value))]
+    # get number of releases per selected value according to year
+    data = data.groupby(['Premiered']).size().reset_index(name='Number of Releases')
+    figure = px.line(data, x='Premiered', y='Number of Releases', title='Number of Releases per Genre')
+    return figure
